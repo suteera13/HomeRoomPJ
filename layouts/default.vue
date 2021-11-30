4ca60e41ae0a62054b6e0f85d47510f4f64a5120
USER <EMAIL>
2021-11-30 09:25:34 +0700

    new project add index,login,signin

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <div class="bg2">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </div>
+  </v-app>
+</template>
+
+<style>
+  .bg{
+    background-image: url("../assets/img/homeroom.png");
+    background-attachment: fixed;
+    background-size: 100% 100%, auto;
+    height: 100%;
+  }
+  .bg2{
+    background-color: #37474F;
+    height: 100%;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a href="login">
+      <v-img src="../img/23.png" class="img" />
+    </a>
+    <p class="text1 text-center">HomeRooM</p>
+    <p class="text2 text-center">chaiyaphum technical college</p>
+  </div>
+</template>
+
+<style>
+  .img{
+    width: 80px;
+    margin: auto;
+    margin-top: 70%;
+  }
+  .text1{
+    font-size: 22px;
+    color: #fff;
+  }
+  .text2{
+    font-size: 8px;
+    color: #BDBDBD;
+    margin-top: -5%;
+  }
+</style>
